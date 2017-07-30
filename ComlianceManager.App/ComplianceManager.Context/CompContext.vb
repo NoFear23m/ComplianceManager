7ec6a29a89cd38417eb3443921bfa0384a362b34
Patschka Sascha <patschka.sascha@live.com>
2017-07-30 15:21:50 +0200

    User und Setting dem Model hinzugefügt, Neue DBSets in Contexts eingefügt

--- ComlianceManager.App/ComplianceManager.Context/CompContext.vb
+++ ComlianceManager.App/ComplianceManager.Context/CompContext.vb
@@ -10,6 +10,8 @@ Public Class CompContext
     Public Overridable Property ComplianceItems As DbSet(Of Model.CompliantItem)
     Public Overridable Property EntryTypes As DbSet(Of Model.EntryType)
     Public Overridable Property Resons As DbSet(Of Model.Reason)
+    Public Overridable Property Users As DbSet(Of Model.User)
+    Public Overridable Property Settings As DbSet(Of Model.Setting)
 
 
 
