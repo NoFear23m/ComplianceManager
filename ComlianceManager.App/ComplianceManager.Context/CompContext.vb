932d1a99acd4e945aa5d68490dd31e1a84161cff
Patschka Sascha <patschka.sascha@live.com>
2017-07-30 15:07:41 +0200

    Settings und User dem Model und Context hinzugefügt

--- ComlianceManager.App/ComplianceManager.Context/CompContext.vb
+++ ComlianceManager.App/ComplianceManager.Context/CompContext.vb
@@ -9,9 +9,10 @@ Public Class CompContext
 
     Public Overridable Property ComplianceItems As DbSet(Of Model.CompliantItem)
     Public Overridable Property EntryTypes As DbSet(Of Model.EntryType)
-    Public Overridable Property Resons As DbSet(Of Model.Reason)
+    Public Overridable Property Reasons As DbSet(Of Model.Reason)
 
-
+    Public Overridable Property Users As DbSet(Of Model.User)
+    Public Overridable Property Settings As DbSet(Of Model.Setting)
 
 
 End Class
---
+++ ComlianceManager.App/ComplianceManager.Model/User.vb
@@ -0,0 +1,35 @@
+﻿
+Imports System.ComponentModel.DataAnnotations
+
+Public Class User
+
+    Public Sub New()
+        UpdatedAt = Now
+        CreatedAt = Now
+        IsAdmin = False
+        IsActive = True
+
+    End Sub
+
+    <Key>
+    Public Overridable Property ID As Integer
+
+    <Required>
+    Public Overridable Property UserName As String
+    <Required>
+    Public Overridable Property FullName As String
+    <Required>
+    Public Overridable Property IsAdmin As Boolean
+    <Required>
+    Public Overridable Property IsActive As String
+    <Required>
+    Public Overridable Property CreatedBy As String
+    <Required>
+    Public Overridable Property UpdatedBy As String
+    <Required>
+    Public Overridable Property UpdatedAt As DateTime
+    <Required>
+    Public Overridable Property CreatedAt As DateTime
+
+
+End Class
